f30db6cf5b06b3ef6aef19954854430aa61d4476
daBoss <serge.loie@yandex.ru>
2024-08-30 23:56:25 +0500

    remove unused imports

--- src/test/java/ru/anseranser/model/TrillionTest.java
+++ src/test/java/ru/anseranser/model/TrillionTest.java
@@ -1,9 +1,6 @@
 package ru.anseranser.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
