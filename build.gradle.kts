81486612407632e329f9501f5084edbef25fa3dd
daBoss <serge.loie@yandex.ru>
2024-09-01 11:34:34 +0500

    Rest test created

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.0"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.0")
 }
 
--- src/test/java/ru/anseranser/NumberControllerTest.java
+++ src/test/java/ru/anseranser/NumberControllerTest.java
@@ -5,26 +5,50 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.anseranser.controller.NumberController;
-import ru.anseranser.dto.NumberInputDTO;
-import ru.anseranser.dto.NumberOutputDTO;
 import ru.anseranser.service.NumberService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class NumberControllerTest {
+    @Autowired
+    private ResourceLoader resourceLoader;
 
-    @Mock
-    private NumberService numberService;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @InjectMocks
-    private NumberController numberController;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Test
+    public void zeroTest() throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:json/1.json");
+        String request = new String(Files.readAllBytes(Paths.get(resource.getURI())));
+        String result = mockMvc.perform(post("/convert")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request)).andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertThatJson(result).and(
+                v -> v.node("number").isEqualTo(1),
+                v -> v.node("gender").isEqualTo("MASCULINE"),
+                v -> v.node("case").isEqualTo("NOMINATIVE"),
+                v -> v.node("numberInWords").isEqualTo("один")
+        );
     }
-
-
 }
